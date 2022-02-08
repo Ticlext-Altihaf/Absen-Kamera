e377664dc60a31a256960bd1ac226499fb33eb89
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2022-02-08 14:50:07 +0000

    fix

--- main.py
+++ main.py
@@ -16,27 +16,37 @@ def load(url):
 print(os.getcwd())
 
 cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
-face_cascade = cv2.CascadeClassifier(os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml'))
+face_cascade = cv2.CascadeClassifier(os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_alt.xml'))
 eye_cascade = cv2.CascadeClassifier(os.path.join(cv2_base_dir, 'data/haarcascade_eye.xml'))
 
 
-
-
-img = load("https://cdn.discordapp.com/attachments/939013407842127882/940591793098088448/3289223.png")
-if(img is None):
-    print("NO IMAGE")
-    sys.exit()
-    
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
+def process(url):
+    img = load(url)
+    if(img is None):
+        raise Exception("Could not load image")
         
-cv2.imwrite("cool.png", img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    print("Found:", len(faces), "faces")
+    name = url.split('/')[-1]
+    name = "faces_" + name
+    counter = 0
+    import shutil
+    try:
+        shutil.rmtree(name)
+    except:
+        pass
+    os.mkdir(name)
+    preview = img.copy()
+    for (x,y,w,h) in faces:
+        sub_img = img[y:y+h, x:x+w]
+        cv2.imwrite(name + "/face_" + str(counter) + ".png", sub_img)
+        counter += 1
+        preview = cv2.rectangle(preview,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imwrite(name+".png", preview)
+
+
+process("https://cdn.discordapp.com/attachments/939013407842127882/940591793098088448/3289223.png")
+
+
 cv2.destroyAllWindows()
